7672499846be55dba894fdf716ff1782ed99bee5
reallyshadydev <dannyt89119@gmail.com>
2025-01-09 15:16:22 -0800

    Update constants.rs
    flopcoin prefix

--- src/blockdata/constants.rs
+++ src/blockdata/constants.rs
@@ -40,8 +40,8 @@ pub const MIN_TRANSACTION_WEIGHT: u32 = 4 * 60;
 pub const WITNESS_SCALE_FACTOR: usize = 4;
 /// The maximum allowed number of signature check operations in a block
 pub const MAX_BLOCK_SIGOPS_COST: i64 = 80_000;
-/// Mainnet (bitcoin) pubkey address prefix.
-pub const PUBKEY_ADDRESS_PREFIX_MAIN: u8 = 0x38;
+/// Mainnet (flopcoin) pubkey address prefix.
+pub const PUBKEY_ADDRESS_PREFIX_MAIN_1: u8 = 0x23; 
 /// Mainnet (bitcoin) script address prefix.
 pub const SCRIPT_ADDRESS_PREFIX_MAIN: u8 = 0x16; // 0x05
 /// Test (tesnet, signet, regtest) pubkey address prefix.
